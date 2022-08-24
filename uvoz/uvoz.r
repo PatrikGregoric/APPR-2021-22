433f0f900c2e548857a95d3167e52f1cbd95437e
Patrik Gregoric <patrik.gregoric@gmail.com>
2022-08-24 17:54:13 +0200

    Ureditev podatkov in dva grafa

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,89 @@
 # 2. faza: Uvoz podatkov
-
+library(tidyverse)
+library(readr)
+library(tidyr)
+library(dplyr)
+library(data.table)
 
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+
+
+BDP_regije = read.csv2(
+  "podatki/Bdp po regijah.csv",
+  skip = 2,
+  fileEncoding = "UTF-8",
+  sep = ";")
+
+BDP_regije.1 = pivot_longer(BDP_regije,
+                            cols = colnames(BDP_regije)[-1],
+                            names_to = "leto",
+                            values_to = "BDP")
+
+BDP_regije.1 = BDP_regije.1 %>%
+  group_by(leto) %>%
+  mutate(leto = str_replace_all(leto, "X{1}", "")) %>%
+  rename(statisticna_regija = STATISTIČNA.REGIJA)
+
+Cene_energentov = read.csv2(
+  "podatki/Cene energentov.csv",
+  skip = 2,
+  fileEncoding = "UTF-8",
+  sep = ";")
+
+Cene_energentov = pivot_longer(Cene_energentov,
+                               cols = colnames(Cene_energentov)[-1],
+                               names_to = "leto",
+                               values_to = "cena")
+
+Cene_energentov = Cene_energentov %>%
+  mutate(leto = str_sub(leto, 2, 5))
+
+Cene_energentov = Cene_energentov %>%
+  group_by(leto) %>%
+  summarise(list(mean = mean(cena, na.rm = TRUE)))
+
+Poraba_gospodinjstev = read.csv(
+  "podatki/Poraba gospodinjstev.csv",
+  skip = 2,
+  fileEncoding = "Windows-1250",
+  sep = ";")
+
+Poraba_gospodinjstev = pivot_longer(Poraba_gospodinjstev,
+                                    cols = colnames(Poraba_gospodinjstev)[-1],
+                                    names_to = "leto",
+                                    values_to = "cena")
+
+Poraba_gospodinjstev = Poraba_gospodinjstev %>%
+  rename(vrsta = ECOICOP) %>%
+  mutate(leto = str_replace_all(leto, "[^0-9]*", ""),
+         vrsta = str_replace_all(vrsta, "^[\\d{1,5} ]*", ""))
+
+Dostop_do_interneta = read.csv(
+  "podatki/Dostop do interneta v gospodinjstvih.csv",
+  skip = 2,
+  fileEncoding = "Windows-1250",
+  sep = ";")
+
+Dostop_do_interneta = Dostop_do_interneta %>%
+  rename(Dostop_do_interneta = DOSTOP.GOSPODINJSTEV.DO.INTERNETA,
+         Stevilo_gospodinjstev = Število.gospodinjstev...SKUPAJ)
+
+Dostop_do_interneta_poskus = pivot_wider(Dostop_do_interneta,
+                                         names_from = Dostop_do_interneta,
+                                         values_from = Stevilo_gospodinjstev)
+
+Prebivalstvo = read.csv(
+  "podatki/Prebivalstvo Slovenije.csv",
+  skip = 2,
+  fileEncoding = "Windows-1250",
+  sep = ";")
+
+Prebivalstvo = Prebivalstvo[,-2] #ker ne potrebujem spola
+
+Prebivalstvo = pivot_longer(Prebivalstvo,
+                            cols = colnames(Prebivalstvo)[-1],
+                            names_to = "leto",
+                            values_to = "stevilo")
+
+Prebivalstvo = Prebivalstvo[,-1] %>% # prvi stolpec nam ne poda nobene informacije
+  mutate(leto = str_replace_all(leto, "X{1}", ""))
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,1 +1,29 @@
 # 3. faza: Vizualizacija podatkov
+
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+#1.graf: Cene energentov
+
+graf_cene_energentov = ggplot(data = Cene_energentov, mapping = aes(x=leto, y=cena, group=ENERGENT, color=ENERGENT)) + 
+  geom_line(stat = "identity", position = "identity") +
+  theme(axis.text.x = element_text(angle = 90,size = 8)) +
+  labs(y = "Cena", color = "Energenti") +
+  theme(legend.text = element_text(size=8)) + 
+  ggtitle("Cene energentov")
+
+plot(graf_cene_energentov)
+#2.graf: BDP regije
+
+graf_BDP = ggplot(data = filter(BDP_regije, statisticna_regija == 'Pomurska' | statisticna_regija == 'Podravska' |
+                                  statisticna_regija == 'Koroška' | statisticna_regija == 'Savinjska' | statisticna_regija == 'Zasavska' |
+                                  statisticna_regija == 'Posavska' | statisticna_regija == 'Jugovzhodna' | statisticna_regija == 'Osrednjeslovenska' |
+                                  statisticna_regija == 'Gorenjska' | statisticna_regija == 'Primorsko-notranjska' |
+                                  statisticna_regija == 'Goriška' | statisticna_regija == 'Obalno-kraška'),
+                  mapping = aes(x=leto, y=BDP, color=statisticna_regija)) +
+                  ggtitle("BDP po regijah") + 
+                  geom_point() +
+                  facet_wrap(statisticna_regija~., ncol=3) +
+                  theme(axis.text.x = element_text(angle = 90, size=6))
+plot(graf_BDP)
